6c7026632c3c800c588235ac371bd26a166313ea
Crowdbotics <team@crowdbotics.com>
2020-05-05 21:43:07 +0000

    Update models

--- backend/lizz_may5_mob1_heal_16640/wsgi.py
+++ backend/lizz_may5_mob1_heal_16640/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lizz_may5_mob1_heal_16640.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lizz_may5_mob1_heal_16640.settings")
 
 application = get_wsgi_application()
